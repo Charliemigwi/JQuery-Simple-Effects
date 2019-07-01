72b1276920198699b594fa4618f5a3e3fc6f0e2a
Charlie Migwi <charliemigwi@gmail.com>
2019-07-01 16:43:07 +0300

    add code to show and hide image

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+/*
 $(document).ready(function() {
   $("p").click(function() {
     $("img").show();
+  });
+});
+*/
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
