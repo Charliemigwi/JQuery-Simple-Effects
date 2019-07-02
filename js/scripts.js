c452f28ff85faf2bfafa8470f08cdfde8ef59413
Charlie Migwi <charliemigwi@gmail.com>
2019-07-02 10:59:12 +0300

    update sscripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
-/*
+
 $(document).ready(function() {
   $("p").click(function() {
     $("img").show();
   });
 });
-*/
+$(document).ready(function() {
+  $(".clickable").click(function() {
+      $("#walrus-showing").toggle();
+      $("#walrus-hidden").toggle();
+    });
+});
+
+/*
 $(document).ready(function() {
   $("p").click(function() {
     $(".walrus-showing").toggle();
     $(".walrus-hidden").toggle();
   });
 });
+*/
